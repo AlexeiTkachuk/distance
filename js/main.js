0f1f00bdf5cf8f302005bdea9f2f6f09104b919e
AlexeiTkachuk <66493495+AlexeiTkachuk@users.noreply.github.com>
2020-06-06 00:00:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const menubtn= document.querySelector(".m-menu");
+const menu=document.querySelector(".menu-sidebar");
+menubtn.addEventListener('click', function(){
+    menu.classList.toggle("active");
+})
